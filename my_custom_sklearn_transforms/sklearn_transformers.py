5d6fa20d8120fdc7967473f6643c1247aa3fa0d8
camiloajt <50209083+camiloajt@users.noreply.github.com>
2020-12-05 14:02:31 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,14 +14,14 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
-
- class ChangeTypeColumns(BaseEstimator, TransformerMixin):
+    
+class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
-    
+
+    def fit(self, X, y=None):
+        return self
+
     def transform(self, X):
         data = X.copy()
         return data.map({0.0: '0'})
-    
-    def fit(self, X, y=None):
-        return self
